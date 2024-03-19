5127deaf32e28cc8456463ecd63f739e7e09b93e
Anju Arianto <anjuarianto@gmail.com>
2024-03-19 15:06:01 +0700

    feat: batasin button add news hanya pada orang yang punya permission

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -35,6 +35,8 @@ export default function Home() {
   const [isOpenRoomModal, setIsOpenRoomModal] = useState(false);
   const [pengumumanIsEdit, setpengumumanIsEdit] = useState<number | null>(null);
   const [reloadPengumuman, setReloadPengumuman] = useState(false);
+  const [calendarData, setCalendarData] = useState<any>();
+  const [myData, setMyData] = useState<any>();
   const openRoomModal = () => {
     setIsOpenRoomModal(true);
   };
@@ -42,6 +44,32 @@ export default function Home() {
   const toggleModalPengumuman = () => {
     setIsModalOpenPengumuman(!isModalOpenPengumuman);
   };
+
+    const loadMyData = async () => {
+        try {
+            const response = await axios.get(
+                "http://127.0.0.1:8000/api/me",
+                {
+                    headers: {
+                        Authorization:
+                            "Bearer " + Cookies.get("accessToken"),
+                    },
+                }
+            );
+
+            setMyData(response.data);
+
+            const pengumumanData = response.data.pengumuman.map((data: any) => ({
+                title: data.judul,
+                start: data.waktu,
+                end: data.waktu,
+            }));
+
+            setCalendarData(pengumumanData);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
   const searchForm = useForm<any>();
 
@@ -51,9 +79,13 @@ export default function Home() {
         loadPengumumanData();
       }
 
-      setReloadPengumuman(false)
+      if(!openCal) {
+        loadMyData();
+      }
+      setReloadPengumuman(false, )
     });
-  }, [isModalOpenPengumuman, reloadPengumuman]);
+
+  }, [isModalOpenPengumuman, reloadPengumuman, openCal]);
 
   const loadPengumumanData = async () => {
     try {
@@ -120,7 +152,7 @@ export default function Home() {
       <Navbar></Navbar>
       <div className="flex flex-col items-center w-full h-full pt-16 ">
 
-        <Toolbar openModalFormPengumuman={toggleModalPengumuman} handleInputChange={handleInputChange} />
+        <Toolbar openModalFormPengumuman={toggleModalPengumuman} handleInputChange={handleInputChange} myData={myData} />
 
         <div className="flex flex-row justify-center w-full h-screen px-12">
 
@@ -175,6 +207,7 @@ export default function Home() {
       <CalendarModal
           openCal={openCal}
           handleClose={handleClose}
+        myCalendarData={calendarData}
       />
 
 

05719568030bb78ab62d94bbae8be230567cb7a7
Anju Arianto <anjuarianto@gmail.com>
2024-03-22 22:00:28 +0700

    fix: upcoming widget update

--- components/Home/UpcomingWidget.tsx
+++ components/Home/UpcomingWidget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Modal } from "@mui/material";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -6,26 +6,28 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 type UpcomingWidgetProps = {
-
+    events: {
+        judul: string;
+        waktu: string;
+    }[];
 };
 
-const UpcomingWidget: React.FC<UpcomingWidgetProps> = () => {
+const UpcomingWidget: React.FC<UpcomingWidgetProps> = ({events}) => {
+
     return (
         <div className="p-6 m-2 bg-white rounded-lg ">
             <h2 className="py-2 text-xl text-center ">Upcoming</h2>
             <div className="divide-y ">
-                <div className="flex flex-col px-2 py-1 ">
-                    <span className="font-bold">Judul 1</span>{" "}
-                    <span className="text-sm">15 January 2024</span>
-                </div>
-                <div className="flex flex-col px-2 py-1 ">
-                    <span className="font-bold">Judul 1</span>{" "}
-                    <span className="text-sm">15 January 2024</span>
-                </div>
-                <div className="flex flex-col px-2 py-1 ">
-                    <span className="font-bold">Judul 1</span>{" "}
-                    <span className="text-sm">15 January 2024</span>
-                </div>
+                {events && events.map((event, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-col px-2 py-1 ">
+                        <span className="font-bold">{event.judul}</span>{" "}
+                        <span className="text-sm">{event.waktu}</span>
+                    </div>
+                ))}
+
+
             </div>
         </div>
     );

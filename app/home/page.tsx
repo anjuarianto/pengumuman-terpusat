444eae762c750ae459f7e115f747082d2a3afd7e
Anju Arianto <anjuarianto@gmail.com>
2024-04-08 20:28:22 +0700

    fix: action open modal pengumuman

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,56 +1,56 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import {useForm} from "react-hook-form";
 import Navbar from "@/components/Navbar";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
-import redirect from "next/navigation";
+import {useRouter} from "next/navigation";
 
 // Home Component
 import RoomList from "@/components/Home/RoomList";
 import ModalRoomList from "@/components/Home/ModalRoomList";
-import  PengumumanList from "@/components/Home/PengumumanList";
+import PengumumanList from "@/components/Home/PengumumanList";
 import PengumumanModal from "@/components/Home/PengumumanModal";
 import Toolbar from "@/components/Home/Toolbar";
-import CalendarModal from "@/components/Home/CalendarModal";
 import UpcomingWidget from "@/components/Home/UpcomingWidget";
 import Calendar from "@/components/Home/Calendar";
+import MyPengumumanModal from "@/components/Home/MyPengumumanModal";
 
 type Pengumuman = {
-  created_by: string;
-  id: number;
-  judul: string;
-  konten: string;
-  penerima: { penerima_id: number; name: string; is_single_user: boolean }[];
-  waktu: string;
-  can_reply: boolean;
-  can_edit: boolean;
-  can_delete: boolean;
+    created_by: string;
+    id: number;
+    judul: string;
+    konten: string;
+    penerima: { penerima_id: number; name: string; is_single_user: boolean }[];
+    waktu: string;
+    can_reply: boolean;
+    can_edit: boolean;
+    can_delete: boolean;
 };
 
 export default function Home() {
-  const router = useRouter();
-  const [pengumuman, setPengumuman] = useState<Pengumuman[]>([]);
-  const [isModalOpenPengumuman, setIsModalOpenPengumuman] = useState(false);
-  const [openCal, setOpenCal] = useState(false);
-  const [isOpenRoomModal, setIsOpenRoomModal] = useState(false);
-  const [pengumumanIsEdit, setpengumumanIsEdit] = useState<number | null>(null);
-  const [reloadPengumuman, setReloadPengumuman] = useState(false);
-  const [calendarData, setCalendarData] = useState<any>();
-  const [upcomingEvent, setUpcomingEvent] = useState<any>();
-  const [myData, setMyData] = useState<any>();
-  const [roomId, setRoomId] = useState<number>(1);
-  const [navigate, setNavigate] = useState("");
+    const router = useRouter();
+    const [pengumuman, setPengumuman] = useState<Pengumuman[]>([]);
+    const [isModalOpenPengumuman, setIsModalOpenPengumuman] = useState(false);
+    const [openCal, setOpenCal] = useState(false);
+    const [isOpenRoomModal, setIsOpenRoomModal] = useState(false);
+    const [pengumumanIsEdit, setpengumumanIsEdit] = useState<number | null>(null);
+    const [reloadPengumuman, setReloadPengumuman] = useState(false);
+    const [calendarData, setCalendarData] = useState<any>();
+    const [upcomingEvent, setUpcomingEvent] = useState<any>();
+    const [myData, setMyData] = useState<any>();
+    const [roomId, setRoomId] = useState<number>(1);
+    const [navigate, setNavigate] = useState("");
+    const [isMyPengumumanOpen, setMyPengumumanOpen] = useState<string | null>();
 
-  const openRoomModal = () => {
-    setIsOpenRoomModal(true);
-  };
+    const openRoomModal = () => {
+        setIsOpenRoomModal(true);
+    };
 
-  const toggleModalPengumuman = () => {
-    setIsModalOpenPengumuman(!isModalOpenPengumuman);
-  };
+    const toggleModalPengumuman = () => {
+        setIsModalOpenPengumuman(!isModalOpenPengumuman);
+    };
 
     const loadMyData = async () => {
         try {
@@ -86,79 +86,78 @@ export default function Home() {
         }
     };
 
-  const searchForm = useForm<any>();
+    const searchForm = useForm<any>();
 
-  useEffect(() => {
-    tokenCheck().then(() => {
-      if(!isModalOpenPengumuman) {
-        loadPengumumanData();
-      }
+    useEffect(() => {
+        tokenCheck().then(() => {
+            if (!isModalOpenPengumuman) {
+                loadPengumumanData();
+            }
 
-      if(navigate) {
-        router.push("/user");
-      }
+            if (navigate) {
+                router.push("/user");
+            }
 
-      if(navigate) {
-        loadMyData();
-      }
+            if (navigate) {
+                loadMyData();
+            }
 
-      if(!openCal) {
-        loadMyData();
-      }
-      setReloadPengumuman(false)
-    });
+            if (!openCal) {
+                loadMyData();
+            }
+            setReloadPengumuman(false)
+        });
 
-  }, [isModalOpenPengumuman, reloadPengumuman, openCal, roomId, navigate]);
+    }, [isModalOpenPengumuman, reloadPengumuman, openCal, roomId, navigate]);
 
-  const loadPengumumanData = async () => {
-    try {
-      const response = await axios.get(
-        "http://127.0.0.1:8000/api/pengumuman",
+    const loadPengumumanData = async () => {
+        try {
+            const response = await axios.get(
+                "http://127.0.0.1:8000/api/pengumuman",
+                {
+                    params: {
+                        search: searchForm.getValues().search,
+                        room_id: roomId,
+                        page: 1,
+                    },
+                    headers: {
+                        Authorization:
+                            "Bearer " + Cookies.get("accessToken"),
+                    },
+                }
+            );
 
-        {
-          params: {
-            search: searchForm.getValues().search,
-            room_id: roomId,
-            page: 1,
-          },
-          headers: {
-            Authorization:
-              "Bearer " + Cookies.get("accessToken"),
-          },
+            const pengumumanData: Pengumuman[] = response.data.data.data;
+
+            setPengumuman(pengumumanData);
+
+        } catch (err) {
+            console.log(err);
         }
-      );
+    };
 
-      const pengumumanData: Pengumuman[] = response.data.data.data;
+    const tokenCheck = async () => {
+        try {
+            const accToken = Cookies.get("accessToken");
+            if (!accToken || accToken.length == 0) {
+                router.push("/login");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-      setPengumuman(pengumumanData);
+    const handleOpen = () => {
+        setIsModalOpenPengumuman(true);
+    };
 
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const tokenCheck = async () => {
-    try {
-      const accToken = Cookies.get("accessToken");
-      if (!accToken || accToken.length == 0) {
-        router.push("/login");
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleOpen = () => {
-    setIsModalOpenPengumuman(true);
-  };
-
-  const handleRoomIdChange = (id: number) => {
-      setRoomId(id);
+    const handleRoomIdChange = (id: number) => {
+        setRoomId(id);
     }
 
-  const handleClose = () => {
-    setOpenCal(false);
-  };
+    const handleClose = () => {
+        setOpenCal(false);
+    };
 
     const handleClosePengumumanModal = () => {
         setIsModalOpenPengumuman(false);
@@ -166,84 +165,91 @@ export default function Home() {
 
     };
 
-  const editForm = async (id: number) => {
-    setpengumumanIsEdit(id);
-  };
+    const editForm = async (id: number) => {
+        setpengumumanIsEdit(id);
+    };
 
 
+    const handleInputChange = (event: any) => {
+        searchForm.setValue('search', event.target.value);
+        loadPengumumanData();
+    };
 
-  const handleInputChange = (event: any) => {
-    searchForm.setValue('search', event.target.value);
-    loadPengumumanData();
-  };
+    return (
+        <>
+            <Navbar>
+            </Navbar>
+            <div className="flex flex-col items-center w-full h-full md:pt-16 ">
 
-  return (
-    <>
-      <Navbar>
-      </Navbar>
-      <div className="flex flex-col items-center w-full h-full md:pt-16 ">
+                <Toolbar
+                    onSubmitSearch={searchForm.handleSubmit(handleInputChange)}
+                    openModalFormPengumuman={toggleModalPengumuman}
+                    handleInputChange={handleInputChange}
+                    myData={myData}
+                    roomActive={roomId}
+                    setRoomId={() => {
+                        setRoomId(1);
+                    }}
+                />
 
-        <Toolbar
-            onSubmitSearch={searchForm.handleSubmit(handleInputChange)}
-            openModalFormPengumuman={toggleModalPengumuman}
-            handleInputChange={handleInputChange}
-            myData={myData}
-            roomActive={roomId}
-            setRoomId={() =>{
-                setRoomId(1);
-            }}
-        />
+                <div className="flex flex-col md:flex-row justify-center w-full md:h-screen  md:px-12 ">
 
-        <div className="flex flex-col md:flex-row justify-center w-full md:h-screen  md:px-12 ">
+                    <RoomList
+                        openModal={openRoomModal}
+                        isModalOpen={isOpenRoomModal}
+                        setRoomId={(id: string) => handleRoomIdChange(parseInt(id))}
+                        roomActive={roomId}
+                    ></RoomList>
 
-          <RoomList
-            openModal={openRoomModal}
-            isModalOpen={isOpenRoomModal}
-            setRoomId={(id: string) => handleRoomIdChange(parseInt(id))}
-            roomActive={roomId}
-          ></RoomList>
-
-          {/* pengumuman */}
-          <PengumumanList
-              pengumuman={pengumuman}
-              editForm={(id) => {
-                toggleModalPengumuman();
-                editForm(id);
-              }}
-              reload={() => {
-                setReloadPengumuman(true);
-              }}
-          />
+                    {/* pengumuman */}
+                    <PengumumanList
+                        pengumuman={pengumuman}
+                        editForm={(id) => {
+                            toggleModalPengumuman();
+                            editForm(id);
+                        }}
+                        reload={() => {
+                            setReloadPengumuman(true);
+                        }}
+                    />
 
 
-          {/* calendar and upcoming */}
-          <div className="flex flex-col w-full md:w-1/5 md:h-screen gap-4 ">
-              <Calendar
-                  myCalendarData={calendarData}
-              />
+                    {/* calendar and upcoming */}
+                    <div className="flex flex-col w-full md:w-1/5 md:h-screen gap-4 ">
+                        <Calendar
+                            openMyPengumuman={(date: string) => {
+                                setMyPengumumanOpen(date);
+                            }}
+                            myCalendarData={calendarData}
+                        />
 
-            <UpcomingWidget events={upcomingEvent}>
+                        <UpcomingWidget events={upcomingEvent}>
 
-            </UpcomingWidget>
-          </div>
-        </div>
-      </div>
+                        </UpcomingWidget>
+                    </div>
+                </div>
+            </div>
 
-      <PengumumanModal
-          isModalOpen={isModalOpenPengumuman}
-          onClose={handleClosePengumumanModal}
-          isEdit={pengumumanIsEdit}
-          roomActive={roomId}
-      ></PengumumanModal>
+            <PengumumanModal
+                isModalOpen={isModalOpenPengumuman}
+                onClose={handleClosePengumumanModal}
+                isEdit={pengumumanIsEdit}
+                roomActive={roomId}
+            ></PengumumanModal>
 
-      <ModalRoomList
-          isOpen={isOpenRoomModal}
-          onClose={() => {
-            setIsOpenRoomModal(false);
-          }}
-      ></ModalRoomList>
+            <ModalRoomList
+                isOpen={isOpenRoomModal}
+                onClose={() => {
+                    setIsOpenRoomModal(false);
+                }}
+            ></ModalRoomList>
 
-          
-    </>
-  );
+            <MyPengumumanModal
+                isOpen={!!isMyPengumumanOpen}
+                date={isMyPengumumanOpen}
+                onClose={() => setMyPengumumanOpen(false)}
+            />
+
+        </>
+    );
 }
---
+++ components/Home/MyPengumumanModal.tsx
@@ -0,0 +1,103 @@
+'use client';
+import axios from "axios";
+import React, {useEffect} from "react";
+import {Button, Modal, Paper, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import Cookies from "js-cookie";
+
+const API_URL = 'http://127.0.0.1:8000/api/my-pengumuman/';
+const HEADERS = {
+    headers: {
+        Authorization: "Bearer " + Cookies.get("accessToken"),
+    }
+};
+
+type ModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+    date: string;
+};
+
+type DataItem = {
+    id: number;
+    title: string;
+    waktu: string;
+};
+
+export default function MyPengumumanModal({isOpen, onClose, date}: ModalProps) {
+    const [data, setData] = React.useState<DataItem[]>([]);
+
+    const fetchData = async (date: string) => {
+        const response = await axios.get(API_URL + date, HEADERS);
+
+        const dataItem: DataItem[] = response.data.data.map((item: any) => {
+            return {
+                id: item.id,
+                title: item.judul,
+                waktu: item.waktu
+            }
+        });
+
+        setData(dataItem);
+    }
+
+    const handleButtonClick = (data: DataItem) => async () => {
+        const url = `/home/${data.id}`;
+        window.open(url, "_blank");
+    }
+
+    useEffect(() => {
+        isOpen && fetchData(date);
+    }, [isOpen]);
+
+    return (
+        <Modal open={isOpen}>
+            <div
+                className="flex flex-col items-center justify-center h-screen"
+                onClick={onClose}
+            >
+                <div
+                    className="flex flex-col items-center w-full md:w-4/5 h-4/5 md:max-h-screen overflow-y-auto bg-white rounded-lg shadow-lg"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                >
+                    <div
+                        className="w-full h-fit py-4 text-2xl font-bold text-center text-white rounded-t-lg bg-dark-blue ">
+                        Daftar Agenda
+                    </div>
+                    <div className="w-full px-4 md:px-24 py-4">
+                        <Paper>
+                            <Table className="borderless">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>No.</TableCell>
+                                        <TableCell>Title</TableCell>
+                                        <TableCell>Waktu</TableCell>
+                                        <TableCell></TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {data.map((item: DataItem, index) => (
+                                        <TableRow key={item.id}>
+                                            <TableCell>{index + 1}</TableCell>
+                                            <TableCell>{item.title}</TableCell>
+                                            <TableCell>{item.waktu}</TableCell>
+                                            <TableCell>
+                                                <Button
+                                                    onClick={handleButtonClick(item)}
+                                                    variant="contained"
+                                                    color="primary">
+                                                    Open
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Paper>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    )
+}

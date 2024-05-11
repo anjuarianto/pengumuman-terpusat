14629172aca335949b9b7a3f17cda80906b5c348
Anju Arianto <anjuarianto@gmail.com>
2024-05-11 15:50:52 +0700

    fix: ganti url api

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.API_URL + "/:path*",
+        destination: process.env.API_URL+ "api/:path*",
       },
       {
         source: "/storage/:path*",
